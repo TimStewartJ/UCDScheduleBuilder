35485e98283a495955b69694ac9e6a1ac7553546
Tim <timstewartj@gmail.com>
2020-05-09 18:08:32 +0900

    added js query form

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-
-function input(){
-  input = document.getElementById('input').value;
-  document.getElementById('output').innerHTML = input;
-}
+$(document).ready(function() {
+  $("div#form").append(
+    $("<h3/>").text("this is test text"), $("<p/>").text("More test tesxt"), $("<form/>",{
+      action:'#',
+      method:'#'
+    }).append(
+      $("<input/>", {
+        type: 'text',
+        id: 'inputID',
+        name: 'input',
+        placeholder: 'MAT 021A,DICK 069'
+      }), $("<br/>"), $("<input/>", {
+        type: 'submit',
+        id: 'submit',
+        value: 'Submit'
+      }))
+    )
+});
